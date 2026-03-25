795edaa5537faeb3b34b21078187e1ebbe43b9e9
Chinmay Manjunath <chinmayhegday@gmail.com>
2026-03-24 19:46:55 -0700

    support --port through cobra CLI

--- cmd/homeapi/main.go
+++ cmd/homeapi/main.go
@@ -9,32 +9,51 @@ import (
 
 	"github.com/chinmay28/homeapi/internal/api"
 	"github.com/chinmay28/homeapi/internal/db"
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	port := os.Getenv("HOMEAPI_PORT")
-	if port == "" {
-		port = "8080"
+	var port string
+
+	rootCmd := &cobra.Command{
+		Use:   "homeapi",
+		Short: "HomeAPI server",
+		Run: func(cmd *cobra.Command, args []string) {
+			// Resolve port: flag > env > default
+			if port == "" {
+				port = os.Getenv("HOMEAPI_PORT")
+			}
+			if port == "" {
+				port = "8080"
+			}
+
+			dbPath := os.Getenv("HOMEAPI_DB_PATH")
+
+			store, err := db.New(dbPath)
+			if err != nil {
+				log.Fatalf("Failed to initialize database: %v", err)
+			}
+			defer store.Close()
+
+			handler := api.NewHandler(store)
+
+			var frontendFS fs.FS
+			frontendFS = getEmbeddedFrontend()
+
+			router := api.NewRouter(handler, frontendFS)
+
+			addr := fmt.Sprintf(":%s", port)
+			log.Printf("HomeAPI starting on http://localhost%s", addr)
+
+			if err := http.ListenAndServe(addr, router); err != nil {
+				log.Fatalf("Server failed: %v", err)
+			}
+		},
 	}
 
-	dbPath := os.Getenv("HOMEAPI_DB_PATH")
+	rootCmd.Flags().StringVar(&port, "port", "", "Port to run the server on (overrides HOMEAPI_PORT)")
 
-	store, err := db.New(dbPath)
-	if err != nil {
-		log.Fatalf("Failed to initialize database: %v", err)
-	}
-	defer store.Close()
-
-	handler := api.NewHandler(store)
-
-	var frontendFS fs.FS
-	frontendFS = getEmbeddedFrontend()
-
-	router := api.NewRouter(handler, frontendFS)
-
-	addr := fmt.Sprintf(":%s", port)
-	log.Printf("HomeAPI starting on http://localhost%s", addr)
-	if err := http.ListenAndServe(addr, router); err != nil {
-		log.Fatalf("Server failed: %v", err)
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatal(err)
 	}
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,9 @@ module github.com/chinmay28/homeapi
 go 1.24.7
 
 require github.com/mattn/go-sqlite3 v1.14.37
+
+require (
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/spf13/cobra v1.10.2 // indirect
+	github.com/spf13/pflag v1.0.9 // indirect
+)
